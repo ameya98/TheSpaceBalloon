2031023aaf28a7a1dbd34aa0edad4c40092f3395
Ameya Daigavane <ameya.d.98@gmail.com>
2019-03-15 17:45:10 +0530

    added waiting between intervals

--- camera/camera.py
+++ camera/camera.py
@@ -3,8 +3,29 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
-sleep(10)
-camera.stop_recording()
-camera.stop_preview()
+# Length of video capture in seconds.
+video_length = 10
+
+# Approximately how long (in seconds) to wait in between captures.
+interval_length = 1000
+
+# Take video recordings and still image captures.
+for iteration in range(1, 101):
+
+    # Video recording.
+    camera.start_preview()
+    sleep(2)
+    camera.start_recording('/home/pi/video' + str(iteration) + '.h264')
+    sleep(video_length)
+    camera.stop_recording()
+    camera.stop_preview()
+
+    # Still image capture.
+    camera.start_preview()
+    sleep(2)
+    camera.start_recording('/home/pi/img' + str(iteration) + '.jpg')
+    camera.stop_recording()
+    camera.stop_preview()
+
+    # Wait for interval.
+    sleep(interval_length)
